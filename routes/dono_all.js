4f376c07e7eef1757237b5cc0ac147ca1e125f68
Cesar-MLW <cesarmlwestphal@gmail.com>
2018-05-20 19:00:07 -0300

    Fixed

--- routes/dono_all.js
+++ routes/dono_all.js
@@ -4,6 +4,7 @@ module.exports = app => {
     app.route("/dono_all")
         .get((req, res) => {
             app.db.connection.query("SELECT * FROM u179156626_anima.dono_all").spread((results, metadata) => {
+                if(results["nome"] === "Gabriel Nori" || results["José"]) console.log("Funfou");
                 res.send({dono_all: results});
             })
         })
--- routes/vac_all.js
+++ routes/vac_all.js
@@ -1,9 +1,9 @@
-var sequelize = require('../db.js')
+var db = require('../db.js')
 
 module.exports = app => {
     app.route("/vac_all")
         .get((req, res) => {
-            sequelize.query("SELECT * FROM u179156626_anima.vacina_all").spread((results, metadata) => {
+            app.db.connection.query("SELECT * FROM u179156626_anima.vacina_all").spread((results, metadata) => {
                 res.send({vacina: results})
             })
         })
