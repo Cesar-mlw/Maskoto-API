dfb9f147a7a03ac03ceb1591cb5103a93b1ccd82
Cesar-MLW <cesarmlwestphal@gmail.com>
2018-05-21 12:06:15 -0300

    Started the POST requisition

--- routes/dono_all.js
+++ routes/dono_all.js
@@ -4,8 +4,8 @@ module.exports = app => {
     app.route("/dono_all")
         .get((req, res) => {
             app.db.connection.query("SELECT * FROM u179156626_anima.dono_all").spread((results, metadata) => {
-                if(results["nome"] === "Gabriel Nori" || results["José"]) console.log("Funfou");
                 res.send({dono_all: results});
+                console.log(results.id);
             })
         })
 }
---
+++ routes/valida_usuario.js
@@ -0,0 +1,8 @@
+let db = require("../db.js")
+
+module.exports = app => {
+    app.route("/validate_user")
+        .post((req, res) => {
+                //app.db.connection.fn('u179156626_anima.valida_usuario', req.body.email, req.body.senha)
+            })
+        }
