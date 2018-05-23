71fc3c353b9785cfb28e6d21550a20fd8225559f
Cesar-MLW <cesarmlwestphal@gmail.com>
2018-05-23 12:15:33 -0300

    Insert-user.js working!

--- routes/insert-user.js
+++ routes/insert-user.js
@@ -3,9 +3,9 @@ var db = require("../db.js")
 module.exports = app => {
     app.route("/insert-user")
         .post((req, res) => {
-            db.connection.query('select u179156626_anima.insert_user(:nome,:telefone,:senha,:email,:email,:tipo) AS "id";',
+            app.db.connection.query('select u179156626_anima.insert_user(:nome,:telefone,:senha,:email,:email,:tipo) AS "id";',
                 { replacements: { nome: req.body.nome, doc: req.body.doc, telefone: req.body.telefone, senha: req.body.senha, email: req.body.email, tipo: req.body.tipo } }).spread((results, metadata) => {
-                    res.send({user_insert: reuslts})
+                    res.send({user_insert: results})
                 })
         })
 }
