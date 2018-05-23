d201a72438367c7563daeaf5d8141d58205cef53
Cesar-MLW <cesarmlwestphal@gmail.com>
2018-05-23 11:57:22 -0300

    corrected errors in both insert files

--- routes/insert-pet.js
+++ routes/insert-pet.js
@@ -3,7 +3,7 @@ var db = require("../db.js")
 module.exports = app => {
     app.route("/insert-pet/")
         .post((req, res) => {
-            db.connection.query("CALL u179156626_anima.insert_pet(?,?,?,?,?,?,?,?)",
+            db.connection.query("CALL u179156626_anima.insert_pet(:nome,:nascimento,:raca,:pelagem,:cor,:sexo,:dono,:especie)",
                 { replacements: { nome: req.body.nome, nascimento: req.body.nascimento, raca: req.body.int, pelagem: erq.body.pelagem, cor: req.body.cor, sexo: req.body.sexo, dono: erq.body.dono, especie: req.body.especie } })
                 .spread((results, metadata) => {
                     res.send({insert_pet: results})
