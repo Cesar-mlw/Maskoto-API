73054a10d88106f30fe07f9d8df7898f0f902c68
Cesar-MLW <cesarmlwestphal@gmail.com>
2018-05-23 10:47:43 -0300

    Fixed the user validation route

--- routes/valida_usuario.js
+++ routes/valida_usuario.js
@@ -1,7 +1,7 @@
 let db = require("../db.js")
 
 module.exports = app => {
-    app.route("/validate_user")
+    app.route("/validate_user/:email/:senha")
         .post((req, res) => {
                 app.db.connection.query('SELECT u179156626_anima.valida_usuario(:email, :senha) as "validate"', {replacements: {email: req.body.email, senha: req.body.senha} }).spread((results, metadata) => {
                     res.send({autenticate: results})
